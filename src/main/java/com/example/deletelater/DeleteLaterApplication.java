e6d33cbda30a35816f56d9da7479c0295be604a9
Pasha Fentisov <pasha16.ua@gmail.com>
2023-11-16 01:12:51 +0200

    fixed bugs

--- src/main/java/com/example/deletelater/DeleteLaterApplication.java
+++ src/main/java/com/example/deletelater/DeleteLaterApplication.java
@@ -2,12 +2,42 @@ package com.example.deletelater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class DeleteLaterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DeleteLaterApplication.class, args);
     }
+    @GetMapping
+    public String method(){
+        return "index";
+    }
+    @GetMapping("/congrats.html")
+    public String method2(){
+        return "congrats";
+    }
+    @GetMapping("/chatGpt.html")
+    public String method3(){
+        return "chatGpt";
+    }
+
+    @GetMapping("/confirmation.html")
+    public String method4(){
+        return "confirmation";
+    }
+
+    @GetMapping("/final.html")
+    public String method5(){
+        return "final";
+    }
+    @GetMapping("/reminder.html")
+    public String method6(){
+        return "reminder";
+    }
+
 
 }
